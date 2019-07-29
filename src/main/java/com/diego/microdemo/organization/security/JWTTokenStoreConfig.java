36b79e975ada160340efb2466f792c097a3c666d
diegochavezcarro <diegochavezcarro@gmail.com>
2019-07-29 11:40:02 -0300

    authentication andando, pero no andan los roles

--- src/main/java/com/diego/microdemo/organization/security/JWTTokenStoreConfig.java
+++ src/main/java/com/diego/microdemo/organization/security/JWTTokenStoreConfig.java
@@ -1,15 +1,22 @@
 package com.diego.microdemo.organization.security;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import com.diego.microdemo.organization.config.ServiceConfig;
+
+import io.micrometer.core.instrument.util.IOUtils;
 
 @Configuration
 public class JWTTokenStoreConfig {
@@ -36,8 +43,18 @@ public class JWTTokenStoreConfig {
 	// JWT
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
-		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-		converter.setSigningKey(serviceConfig.getJwtSigningKey());
+		//JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		//converter.setSigningKey(serviceConfig.getJwtSigningKey());
+	    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+	    Resource resource = new ClassPathResource("public.txt");
+	    String publicKey = null;
+	    try {
+	        publicKey = IOUtils.toString(resource.getInputStream());
+	    } catch (final IOException e) {
+	        throw new RuntimeException(e);
+	    }
+	    converter.setVerifierKey(publicKey);
+	   
 		return converter;
 	}
 
--- src/main/java/com/diego/microdemo/organization/security/ResourceServerConfiguration.java
+++ src/main/java/com/diego/microdemo/organization/security/ResourceServerConfiguration.java
@@ -1,16 +1,29 @@
 package com.diego.microdemo.organization.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 
 @Configuration
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	private DefaultTokenServices tokenServices;
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/v1/organizations/**").hasRole("ADMIN").anyRequest()
-				.authenticated();
+		//http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/v1/organizations/**").hasRole("ADMIN").anyRequest()
+		//		.authenticated();
+		http.authorizeRequests().anyRequest().hasRole("ADMIN");
+	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenServices(tokenServices);
+		resources.resourceId("account");
 	}
 }
